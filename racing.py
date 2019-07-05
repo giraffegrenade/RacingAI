5c4b897a96074dcbac48da7f7bf3ea14166ca88a
as14g18 <as14g18@soton.ac.uk>
2019-07-05 19:54:19 +0100

    Initial commit

---
+++ racing.py
@@ -0,0 +1,35 @@
+import pygame
+
+SIZE_X = 1000
+SIZE_Y = 600
+
+if __name__ == '__main__':
+    pygame.font.init()
+    pygame.init()
+    pygame.display.set_caption('pyBullethell')
+
+    screen = pygame.display.set_mode((SIZE_X, SIZE_Y))
+    clock = pygame.time.Clock()
+
+    game = Game(SIZE_X, SIZE_Y)
+    game_exit = False
+    while not game_exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_exit = True
+        foreground = pygame.Surface((SIZE_X, SIZE_Y), pygame.SRCALPHA)
+        foreground.fill(pygame.Color('black'))
+    
+        keys = pygame.key.get_pressed()
+
+        game.tick()
+        game.draw(foreground)
+
+        screen.fill((60, 70, 90))
+        screen.blit(foreground, (0, 0))
+        
+        pygame.display.flip()
+
+        clock.tick(60)
+
+    pygame.quit()
